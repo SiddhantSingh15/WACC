4abdd3935fe899f1e2550b8ab3533310ed79ac4e
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-02-01 15:22:48 +0000

    fix: AST compilation is now successful.

--- src/test/scala/Compiler.scala
+++ src/test/scala/Compiler.scala
@@ -1,6 +1,7 @@
 import parsley.Parsley, Parsley._
+import org.scalatest.funsuite.AnyFunSuite
 
-class LexerTest extends org.scalatest.funsuite.AnyFunSuite {
+class LexerTest extends AnyFunSuite {
   test("LexerParsesInteger") {
     val newParser = lexer.INTEGER;
     assert(newParser.parse("5").isSuccess)
