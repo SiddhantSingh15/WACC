d6bdbc9e474d84cbef153ae7d4c1b49813126fb5
lyonely <z.y.f.9955@gmail.com>
2022-02-01 14:04:12 +0000

    [yz10519] feat: Fixed typo errors, changed comment to commentLine and added import for implicitConversions

--- src/main/scala/WACCLexer.scala
+++ src/main/scala/WACCLexer.scala
@@ -1,5 +1,5 @@
-import parsley.Parsley, Parlsey._
-import java.util.function.Predicate
+import parsley.Parsley, Parsley._
+import scala.language.implicitConversions
 
 
 object lexer {
@@ -9,58 +9,50 @@ object lexer {
     import parsley.implicits.character.{stringLift, charLift}
     import parsley.implicits.zipped.Zipped3
     import parsley.errors.combinator.ErrorMethods
-}
 
+    private val wacc = LanguageDef.plain.copy(
+        commentLine = "#",
+        nestedComments = false,
+        keywords = Set("begin", "end", "is", "skip", "read", "free",
+                        "return", "exit", "print", "println", "if", 
+                        "then", "else", "fi", "while", "do", "done",
+                        "begin", "end", "newpair", "call"
+                        ),
+        operators = Set("!", "-", "len", "ord", "chr", "*",
+                        "/", "%", "+", "-", ">", ">=", "<",
+                        "<=", "==", "!=", "&&", "||", "fst", "snd"),
+        identLetter = Predicate(c => c.isLetter || c == '_'),
+        space = Predicate(c => c == ' ' || c == '\t' || c == '\n')
+    )
 
-private val wacc = LangugeDef.plain.copy(
-    comment = "#",
+    private val lexer = new Lexer(wacc)
 
-    nestedComments = false,
+    private val identifierTail = many(alphaNum <|> '_')
 
-    keywords = Set("begin", "end", "is", "skip", "read", "free",
-                    "return", "exit", "print", "println", "if", 
-                    "then", "else", "fi", "while", "do", "done",
-                    "begin", "end", "newpair", "call"
-                    ),
+    private def idMaker(leading : Parsley[Char]) =
+        (leading, identifierTail, many('\'')).zipped {
+            (first, middle, primes) => s"$first${middle.mkString}${primes.mkString}"
+        }
 
-    operators = Set("!", "-", "len", "ord", "chr", "*",
-                    "/", "%", "+", "-", ">", ">=", "<",
-                    "<=", "==", "!=", "&&", "||", "fst", "snd"),
-    
-    //not sure if this is correct
-    identLetter = Predicate(c => c.isLetter || c == '_')
-
-    space = Predicate(c => c == ' ' || c == '\t' || c == '\n')
-)
-
-private val lexer = new Lexer(wacc)
-
-private val identifierTail = many(alphaNum <|> '_')
-
-private def idMaker(leading : Parsley[Char]) =
-    (leading, identifierTail, many('\'')).zipped {
-        (first, middle, primes) => s"$first${middle.mkString}${primes.mkString}"
+    val CON_ID = idMaker(upper)
+    val VAR_ID = idMaker(lower).filterOut {
+        case k if wacc.keywords(k) => s"keyword $k cannot be used as an identifier"
     }
 
-val CON_ID = idMaker(upper)
-val VAR_ID = idMaker(lower).filterOut {
-    case k if wacc.keywords(k) => s"keyword $k cannot be used as an identifier"
-}
+    val INTEGER = lexer.natural
+    val STRING = lexer.stringLiteral
+    val CHAR = lexer.charLiteral
 
-val INTEGER = lexer.natural
-val STRING = lexer.stringLiteral
-val CHAR = lexer.charLiteral
+    val NEWLINE = void(lexer.lexeme(newline))
 
+    def fully[A](p : Parsley[A]): Parsley[A] = 
+        lexer.whiteSpace *> p <* eof
 
-val NEWLINE = void(lexer.lexeme(newline))
-
-def fully[A](p : Parlsey[A]): Parlsey[A] = 
-    lexer.whiteSpace *> p <* eof
-
-object implicits {
-    implicit def implicitToken(s : String): Parsley[Unit] = {
-        if (wacc.keywords(s))        lexer.keyword(s)
-        else if(wacc.operators(s))   lexer.mapOp(s)
-        else                         void(lexer.symbol_(s))
+    object implicits {
+        implicit def implicitToken(s : String): Parsley[Unit] = {
+            if (wacc.keywords(s))        lexer.keyword(s)
+            else if(wacc.operators(s))   lexer.maxOp(s)
+            else                         void(lexer.symbol_(s))
+        }
     }
 }
