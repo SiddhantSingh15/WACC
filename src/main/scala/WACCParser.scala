f3e99fde6e57bdef386b44a2c319acd22dd9e9f5
tg1120 <tg1120@ic.ac.uk>
2022-02-03 10:25:57 +0000

    tg1120 chore : added check for stat termination

--- src/main/scala/WACCParser.scala
+++ src/main/scala/WACCParser.scala
@@ -85,5 +85,14 @@ object parser {
     val param: Parsley[Param] = lift2(Param, types, identifier) ? "param"
 
     val paramList: Parsley[ParamList] = ParamList <#> sepBy1(param, ",") ? "param-list"
+
+    private def statTerminates(stat: Stat): Boolean = stat match {
+    case If(_, s1, s2)       => statTerminates(s1) && statTerminates(s2)
+    case While(_, s)         => statTerminates(s)
+    case Begin(s)            => statTerminates(s)
+    case Seq(s)              => statTerminates(s.last)
+    case Exit(_) | Return(_) => true
+    case _                   => false
+  }
 }
 
