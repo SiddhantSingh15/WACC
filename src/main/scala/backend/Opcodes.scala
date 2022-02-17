c07e0d6a5af4444bb774abe7bf37888b8dc2dab8
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-02-17 17:31:32 +0000

    [ss5120] feat: added more opcodes and arguments.

--- src/main/scala/backend/Opcodes.scala
+++ src/main/scala/backend/Opcodes.scala
@@ -1,21 +1,34 @@
 package backend
 
+import backend.Operand
+import scala.collection.mutable.ListBuffer
+
 object Opcodes {
     sealed trait Opcode 
 
     // ARITHMETIC
 
-    case class Add() extends Opcode
-    case class Sub() extends Opcode
-    case class Mul() extends Opcode
+    case class Add(rd: Register, rn: Register, op2: Operand) extends Opcode
+    case class Sub(rd: Register, rn: Register, op2: Operand) extends Opcode
+    case class Mul(rd: Register, rm: Register, rs: Register) extends Opcode
 
     // COMPARE
 
-    case class Cmp() extends Opcode
+    case class Cmp(rn: Register, op2: Operand) extends Opcode
 
     // BRANCH
 
-    case class B() extends Opcode
-    case class Bl() extends Opcode
-    
+    case class B(label: String) extends Opcode
+    case class Bl(label: String) extends Opcode
+
+    // LOGICAL
+
+    case class Mov(rd: Register, op2: Operand) extends Opcode
+    case class And(rd: Register, rn: Register, op2: Operand) extends Opcode
+    case class Xor(rd: Register, rn: Register, op2: Operand) extends Opcode
+    case class Or(rd: Register, rn: Register, op2: Operand) extends Opcode
+
+    // PUSH/ POP
+    case class Push(regList: ListBuffer[Register]) extends Opcode
+    case class Pop(regList: ListBuffer[Register]) extends Opcode
 }
