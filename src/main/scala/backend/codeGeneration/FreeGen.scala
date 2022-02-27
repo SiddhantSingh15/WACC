5e8a7a85d4067de81a49bc9eeb3bae0c66f52298
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-02-27 22:03:42 +0000

    [ss5120] feat: added `Ldr` and `Mov`.

--- src/main/scala/backend/codeGeneration/FreeGen.scala
+++ src/main/scala/backend/codeGeneration/FreeGen.scala
@@ -5,13 +5,32 @@ import frontend.AST
 import backend.Opcodes.Instr
 import backend.CodeGen._
 import frontend.SymbolTable
+import frontend.AST.{Expr, Ident}
+import backend.Operand
+import backend.Opcodes.{Ldr, Mov}
+import backend.Operand.{R13_SP}
 
 object FreeGen {
 
-  def transFree(expr: AST.Expr): ListBuffer[Instr] = {
+  def transFree(expr: Expr): ListBuffer[Instr] = {
     val instructions = ListBuffer.empty[Instr]
-    val freeRegister = freeRegs()
+    expr match {
+      case id: Ident => 
+        val freeRegister = freeRegs()
+        val (i, t) = symbTable(id)
 
-    val (i, t) = SymbolTable(expr)
+        instructions += Ldr(freeRegister, R13_SP)
+        instructions += Mov(resultRegister, freeRegister)
+
+        addReg(freeRegister)
+
+        t match {
+          
+        }
+
+      case _ => 
+    }
+
+    instructions
   }
 }
