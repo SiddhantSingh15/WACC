db306f83de5886e386112336787df075630320c3
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-02-27 14:25:41 +0000

    [ss5120] feat: added a data table file.

---
+++ src/main/scala/backend/tableDataTypes/DataTable.scala
@@ -0,0 +1,39 @@
+package backend.tableDataTypes
+
+import scala.collection.mutable.ListBuffer
+import backend.Opcodes._
+import frontend.AST.StrLiter
+
+object DataTable {
+  var table = ListBuffer.empty[Data]
+  var size = 0
+
+  def addData(str: StrLiter): Label = {
+    val label = Label(getNext())
+    table += Data(label, str.toString())
+    label
+  }
+
+  private def getNext(): String ={
+    val label = "data:" + size.toString
+    size += 1
+    label
+  }
+
+  def addLabel(label: String, str: String): Label = {
+    val l = Label(label)
+    if (!contains(l)) {
+      table += Data(l, str)
+    }
+    l
+  }
+
+  private def contains(label: Label): Boolean = {
+    for (Data(l, _) <- table) {
+      if (label == l) {
+        return true
+      }
+    }
+    return false
+  }
+}
