f59cb184ec3f4a9bf4aaa85fbd1271c3b3b21d3f
na920 <na920@ic.ac.uk>
2022-02-28 22:02:27 +0000

    [na920] feat: made progress on Assignments

--- src/main/scala/backend/codeGeneration/Assignments.scala
+++ src/main/scala/backend/codeGeneration/Assignments.scala
@@ -6,6 +6,7 @@ import backend.Opcodes._
 import backend.CodeGen._
 import frontend.SymbolTable
 import backend.codeGeneration.ExpressionGen._
+import backend.codeGeneration.PairsGen._
 
 import scala.collection.mutable.ListBuffer
 
@@ -45,9 +46,11 @@ object Assignments {
                 instrs ++= newInstrs
                 val spOffset = currSP - index  
                 instrs += Str(isByte, freeRegister, R13_SP, spOffset)
-            case Fst(id : Ident) => null //TODO
-            case Snd(id : Ident) => null //TODO
-            case ArrayElem(ident, exprList) => null //TODO
+            case Fst(id : Ident) => 
+                instrs ++= transPairAssign(rhs, id, 0, freeRegister)
+            case Snd(id : Ident) => 
+                instrs ++= transPairAssign(rhs, id, 1, freeRegister)
+            case x@ArrayElem(ident, exprList) => null //TODO
             case _ => null  
         }
         addBackRegister(freeRegister)
@@ -63,11 +66,20 @@ object Assignments {
         rhs match {
             case expr : Expr => instrs ++= transExp(expr, freeRegister)
 
-            case Fst(id) => null //TODO
-            case Snd(id) => null //TODO
-            case Call(ident, argList) => null //TODO
-            case ArrayLiter(list) => null //TODO
-            case NewPair(exprOne, exprTwo) =>  null //TODO 
+            case Fst(id : Ident) => 
+                instrs ++= transPairElem(id, 0, freeRegister)
+                instrs ++= loadPairElem(id, freeRegister, 0)
+
+            case Snd(id : Ident) => 
+                instrs ++= transPairElem(id, 1, freeRegister)
+                instrs ++= loadPairElem(id, freeRegister, 1)
+
+            case Call(ident, argList) =>
+                instrs ++= translateCall(ident, argList, freeRegister)
+            case ArrayLiter(list) => 
+                instrs ++= translateArrayLiter(t, list, freeRegister)
+            case NewPair(exprOne, exprTwo) =>  
+                instrs ++= assignRHSPair(t, fst, snd, freeRegister)
         }
         (isByte(t), instrs)
     }
