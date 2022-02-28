ca94d2d3e2d7b18cb348c2dadd45c4511c74e1b0
na920 <na920@ic.ac.uk>
2022-02-28 19:09:52 +0000

    [na920] refactor: RuntimeErrors

--- src/main/scala/backend/DefinedFuncs/RuntimeErrors.scala
+++ src/main/scala/backend/DefinedFuncs/RuntimeErrors.scala
@@ -24,21 +24,18 @@ object RuntimeErrors {
 
   private val ERROR_EXIT_CODE = -1
 
-  /* Adds a runtime error ERR to the function table. */
   def addRuntimeError(err: PreDefFunc): Label = {
     funcTable.addFunction(RuntimeError.func)
     funcTable.addFunction(stringPrintInstrs)
     dataTable.addLabel("msg_string", "%.*s\\0")
-    for(i <- 0 until err.msgs.length){
-      dataTable.addLabel(err.msgName(i), err.msgs(i))
+    for(n <- 0 until err.msgs.length){
+      dataTable.addLabel(err.msgName(n), err.msgs(n))
     }
     funcTable.addFunction(err.func)
     err.funcLabel
 
   }
 
-  /* Throws a runtime error by adding the appropiate instructions to the
-     internal representation. */
   def throwRuntimeError: (Label, ListBuffer[Instr]) = {
     (
       RuntimeError.funcLabel,
@@ -50,8 +47,6 @@ object RuntimeErrors {
     )
   }
 
-  /* Checks the bounds of a given array, branching to a runtime error if
-     necessary. */
   def checkArrayBounds: (Label, ListBuffer[Instr]) = {
     (
       ArrayBounds.funcLabel,
@@ -69,8 +64,6 @@ object RuntimeErrors {
     )
   }
 
-  /* Checks an expression to determine whether an expression has been divided
-     by 0, branching to a runtime error if necessary. */
   def checkDivideByZero: (Label, ListBuffer[Instr]) = {
     (
       DivideByZero.funcLabel,
@@ -84,8 +77,6 @@ object RuntimeErrors {
     )
   }
 
-  /* Checks an expression to determine whether an int has overflown, branching
-     to a runtime error if necessary. */
   def throwOverflowError: (Label, ListBuffer[Instr]) = {
     (
       Overflow.funcLabel,
@@ -96,7 +87,6 @@ object RuntimeErrors {
     )
   }
 
-  /* Free a given pair, branching to a runtime error if necessary. */
   def freePair: (Label, ListBuffer[Instr]) = {
     (
       FreePair.funcLabel,
@@ -118,7 +108,6 @@ object RuntimeErrors {
     )
   }
 
-  /* Free a given array, branching to a runtime error if necessary. */
   def freeArray: (Label, ListBuffer[Instr]) = {
     (
       FreeArray.funcLabel,
@@ -133,8 +122,6 @@ object RuntimeErrors {
     )
   }
 
-  /* Checks if attempting to defererence a null pointer, branching to a
-     runtime error if necessary. */
   def checkNullPointer: (Label, ListBuffer[Instr]) = {
     (
       NullPointer.funcLabel,
