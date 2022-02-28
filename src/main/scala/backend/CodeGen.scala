6a260d8bc509c4c1504f17a406e50cac0763c2e9
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-02-28 12:48:36 +0000

    [ss5120] feat: added `PairsGen`

--- src/main/scala/backend/CodeGen.scala
+++ src/main/scala/backend/CodeGen.scala
@@ -22,6 +22,9 @@ object CodeGen {
   var currSP = 0
   private val ERROR = -1
 
+  val SIZE_ADDR = 4
+  val SIZE_PAIR = SIZE_ADDR
+
   final val generalRegisters: ListBuffer[Register] = 
     ListBuffer(R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12)
 
@@ -68,6 +71,10 @@ object CodeGen {
     val register = freeRegisters(0)
     freeRegisters.remove(0)
     register
+  }
+
+  def addFreeReg(reg: Register): Unit = {
+    reg +=: freeRegisters
   }
 
   def restoreReg(reg: Register): Unit = {
