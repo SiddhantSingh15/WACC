fe1b9515102b449ae5cb22bb510c3db6e5bae748
na920 <na920@ic.ac.uk>
2022-02-28 21:22:53 +0000

    [na920] feat: skeleton for Assignments

--- src/main/scala/backend/CodeGen.scala
+++ src/main/scala/backend/CodeGen.scala
@@ -22,6 +22,11 @@ object CodeGen {
   val SIZE_ADDR = 4
   val SIZE_PAIR = SIZE_ADDR
 
+  //rename
+  var scopeSP  = 0
+  var currSP = 0
+  private val ERROR = -1
+
   final val generalRegisters: ListBuffer[Register] = 
     ListBuffer(R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12)
 
