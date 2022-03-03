50e6770aa93fde9462182d757756cca4a68c14a8
tg1120 <tg1120@ic.ac.uk>
2022-03-03 18:22:25 +0000

    tg1120 chore : added comments to Condition

--- src/main/scala/backend/Condition.scala
+++ src/main/scala/backend/Condition.scala
@@ -5,26 +5,27 @@ object Condition {
   sealed trait Condition {
     val oppositeCmp: Condition = null
   }
-
+  //Equal
   case object EQ extends Condition {
     override val oppositeCmp: Condition = NE
   }
-
+  //Not Equal
   case object NE extends Condition {
     override val oppositeCmp: Condition = EQ
   }
-
+  // Less than
   case object LT extends Condition {
     override val oppositeCmp: Condition = GE
   }
-
+  // Less than Equal
   case object LE extends Condition {
     override val oppositeCmp: Condition = GT
   }
+  //Greater Than
   case object GT extends Condition {
     override val oppositeCmp: Condition = LE
   }
-
+  // Greater than Equal
   case object GE extends Condition {
     override val oppositeCmp: Condition = LT
   }
