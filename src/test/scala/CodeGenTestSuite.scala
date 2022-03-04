65ab4585538d3caa222e240ec66da7d8da439334
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-03-04 14:24:19 +0000

    [ss5120] test: adding basic files.

--- src/test/scala/CodeGenTestSuite.scala
+++ src/test/scala/CodeGenTestSuite.scala
@@ -1,6 +1,9 @@
 import scala.collection.mutable.ListBuffer
 import java.io.File
 import scala.io.Source
+import scala.util.matching.Regex
+import scala.sys.process._
+
 object CodeGenTestSuite {
 
   def loadFile(path: String): ListBuffer[File] = {
@@ -13,30 +16,47 @@ object CodeGenTestSuite {
         result ++= loadFile(file.getPath)
       }
     }
-
     result
   }
 
+  def createDummyOutput(file: File): (File, File, ProcessBuilder) = {
+    makeExec(file)
+    val fName = file.getName().replaceAll(".wacc", "")
+    val result = new File(s"$fName.out")
+    val inputs = Source.fromFile(getFileFromPath(file, "wacc_examples/expected/input")).getLines().mkString("\n")
+    val cmd = 
+    if (inputs.isEmpty) {
+      s"qemu-arm -L /usr/arm-linux-gnueabi/ $fName" #> result
+    } else {
+      (s"printf '$inputs'" #| s"qemu-arm -L /usr/arm-linux/gnueabi/ $fName" #> result)
+    }
+
+    (file, result, cmd)
+  }
+
+  private def getFileFromPath(file: File, folder: String): File = {
+    new File (file.getPath().replace("wacc_examples/valid", folder).replace("wacc", "txt"))
+  }
+
+  private def makeExec(file: File): Unit = {
+    val name = file.getPath()
+    s"java -jar target/scala-2.13/wacc.jar $name".!
+    val changeName = file.getName().replaceAll(".wacc", "")
+    s"arm-linux-gnueabi-gcc -o $name -mcpu=arm1176jzf-s -mtune=arm1176jzf-s $name.s".!
+    val exec = new File(file.getName.replaceAll(".wacc", ".s"))
+    exec.delete()
+  }
+
   def checkOutput(file: File, result: File): Boolean = {
-    val expected = getExpected(file)
-    var printed = readLines(result)
-    val expPrinted = readLines(expected)
+    val expected = getFile(file, "wacc_examples/expected")
+    var printed = Source.fromFile(result).getLines().mkString("\n")
+    val expPrinted = Source.fromFile(expected).getLines().mkString("\n")
 
     result.delete
 
     val registerAddr = "0x[0-9a-z]+".r
-
     printed = registerAddr replaceAllIn (printed, "#addr#")
-
     expPrinted.equals(printed)
-  }
-  
-  private def readLines(file: File): String = {
-    Source.fromFile(file).getLines().mkString("\n")
-  }
-
-  private def getExpected(file: File): File = {
-    getFile(file, "expected")
   }
 
   private def getFile(file: File, folder: String): File = {
