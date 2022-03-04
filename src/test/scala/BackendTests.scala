4b4138ab3b13c7151f196701d1d164d1b136a307
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-03-04 14:39:09 +0000

    [ss5120, yz10519] fix: fixing typo in test file.

--- src/test/scala/BackendTests.scala
+++ src/test/scala/BackendTests.scala
@@ -3,7 +3,7 @@ import CodeGenTestSuite._
 
 class BackendTests extends AnyFunSuite {
   test ("Expected Basic Tests") {
-    for (file <- loadFile("wacc_example/valid/basic/")) {
+    for (file <- loadFile("wacc_examples/valid/basic/")) {
       val fName = file.getName
       val (f, output, cmd) = createDummyOutput(file)
       test(s"Expected Test Name: ($fName)") {
