c016fc7b384013c811b8698fc9a4728478df792d
lyonely <z.y.f.9955@gmail.com>
2022-03-11 02:00:35 +0000

    [yz10519] chore: added missing expected output files and fixed backendTests

--- src/test/scala/BackendTests.scala
+++ src/test/scala/BackendTests.scala
@@ -1,10 +1,17 @@
 import org.scalatest.funsuite.AnyFunSuite
 import CodeGenTestSuite._
+import scala.collection.immutable.List
+import java.io.File
 
 class BackendTests extends AnyFunSuite {
-  test ("Expected Basic Tests") {
-    for (file <- loadFile("wacc_examples/valid/basic/")) {
+  
+  val dir = new File("wacc_examples/expected/")
+  val listOfDir = dir.listFiles.map(_.getName).toList
+
+  for (subDir <- listOfDir) {
+    for (file <- loadFile(s"wacc_examples/valid/$subDir")) {
       val fName = file.getName
+      println(s"Testing $fName")
       val (f, output, cmd) = createDummyOutput(file)
       test(s"Expected Test Name: ($fName)") {
         assert(checkOutput(f, output))
