df353dc68c9485d270f1602a30896c17e83d8d6f
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-03-15 11:43:27 +0000

    [ss5120] feat: adding trans case for `calloc`.

---
+++ src/main/scala/backend/codeGeneration/HeapGen.scala
@@ -0,0 +1,18 @@
+package backend.codeGeneration
+
+object HeapGen {
+  def transHeap(tpe: Type, allocType: Heap, reg: Register): ListBuffer[Instr] = {
+    val instructions = ListBuffer.empty[Instr]
+
+    val PointerType(in) = tpe
+
+    allocType match {
+      case Calloc(num, size, _) =>
+        instructions ++= transExp(size, reg)
+        instructions ++= Mov(R1, reg)
+        instructions ++= transExp(num, reg)
+        instructions ++= Mov(resultRegister, reg)
+        instructions ++= Bl(Label("calloc"))
+    }
+  }
+}
