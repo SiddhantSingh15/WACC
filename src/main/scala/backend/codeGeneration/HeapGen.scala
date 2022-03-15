6e0c3823bb2e7bb3001e10f2a1e6c19625938f13
Siddhant Singh <singhsiddhant@hotmail.co.uk>
2022-03-15 11:54:44 +0000

    [ss5120] feat: added error messages for memory allocation.

--- src/main/scala/backend/codeGeneration/HeapGen.scala
+++ src/main/scala/backend/codeGeneration/HeapGen.scala
@@ -1,6 +1,14 @@
 package backend.codeGeneration
 
 object HeapGen {
+
+  private var heap = Map.empty[Ident, Int]
+  private var addresses = Set.empty[Int]
+  private var next = 0
+  private val OVFLOW_RS = 31
+  private val unallocMemErr = "MemErr: invalid, memory not allocated."
+  private val doubleFreeErr = "MemErr: invalid, double free."
+
   def transHeap(tpe: Type, allocType: Heap, reg: Register): ListBuffer[Instr] = {
     val instructions = ListBuffer.empty[Instr]
 
@@ -24,7 +32,10 @@ object HeapGen {
         instructions += Mov(R1, reg)
         instructions ++= transExp(ptr, reg)
         instructions += Mov(resultRegister, reg)
-        instsructions += Bl(Label("realloc"))
+        instructions += Bl(Label("realloc"))
     }
+
+    instructions += Mov(reg, resultRegister)
+    instructions
   }
 }
