a4f813d089ff6a0aa64c5ccd5a47a1866f8c2198
lyonely <z.y.f.9955@gmail.com>
2022-03-18 12:23:47 +0000

    [yz10519] chore: adjusted tabs and spacing in InstrEval.scala

--- src/main/scala/backend/InstrEval.scala
+++ src/main/scala/backend/InstrEval.scala
@@ -13,214 +13,214 @@ import scala.collection.mutable.ListBuffer
 import scala.math._
 
 object InstrEval {
-    val Is_Byte = true
-    val Not_Byte = false
+  val Is_Byte = true
+  val Not_Byte = false
 
-    /* Blocks of instructions that should be ignored */
-    val ignoreBlocks = mutable.ListBuffer.empty[Label]
+  /* Blocks of instructions that should be ignored */
+  val ignoreBlocks = mutable.ListBuffer.empty[Label]
 
-    /* Optimises a block of instructions */
-    def optimiseBlock(
-        block: (Label, BlockInstrs)
-    ): (Label, BlockInstrs) = {
-        val (label, instructions) = block
-        val optimised = mutable.ListBuffer.empty[Instr]
-        val instrs = mutable.ListBuffer.empty[Instr]
-        instrs.addAll(instructions.instrBlock)
-        // Store optimised instructions into optimised
-        optimise(instrs, optimised)
-        val returnBlock = BlockInstrs(optimised)
-        (label, returnBlock)
-    }
+  /* Optimises a block of instructions */
+  def optimiseBlock(
+      block: (Label, BlockInstrs)
+  ): (Label, BlockInstrs) = {
+      val (label, instructions) = block
+      val optimised = mutable.ListBuffer.empty[Instr]
+      val instrs = mutable.ListBuffer.empty[Instr]
+      instrs.addAll(instructions.instrBlock)
+      // Store optimised instructions into optimised
+      optimise(instrs, optimised)
+      val returnBlock = BlockInstrs(optimised)
+      (label, returnBlock)
+  }
 
-    /* Continues optimise recursively */
-    def continueOptimise(
-        cur: Instr,
-        remainingHead: Instr,
-        remainingTail: mutable.ListBuffer[Instr],
-        optimised: mutable.ListBuffer[Instr]
-    ): Unit = {
-        optimised += cur
-        optimise(remainingHead, remainingTail, optimised)
-    }
+  /* Continues optimise recursively */
+  def continueOptimise(
+      cur: Instr,
+      remainingHead: Instr,
+      remainingTail: mutable.ListBuffer[Instr],
+      optimised: mutable.ListBuffer[Instr]
+  ): Unit = {
+      optimised += cur
+      optimise(remainingHead, remainingTail, optimised)
+  }
 
-    /* Overloaded optimise function that is recursively called to store
-     optimised instructions in optimised */
-    def optimise(
-        instructions: mutable.ListBuffer[Instr],
-        optimised: mutable.ListBuffer[Instr]
-    ): Unit = {
-        if (!instructions.isEmpty) {
-            optimise(instructions.head, instructions.tail, optimised)
-        }
-    }
+  /* Overloaded optimise function that is recursively called to store
+    optimised instructions in optimised */
+  def optimise(
+      instructions: mutable.ListBuffer[Instr],
+      optimised: mutable.ListBuffer[Instr]
+  ): Unit = {
+      if (!instructions.isEmpty) {
+          optimise(instructions.head, instructions.tail, optimised)
+      }
+  }
 
-    /* Overloaded optimise function that is recursively called to store
-     optimised instructions in OPTIMISED */
-    def optimise(
-        cur: Instr,
-        instructions: mutable.ListBuffer[Instr],
-        optimised: mutable.ListBuffer[Instr]
-    ): Unit = {
-        if (instructions.isEmpty) {
-            optimised += cur
-        } else {
-            val remainingHead = instructions.head
-            val remainingTail = instructions.tail
-            (cur, remainingHead) match {
-                case (Mov(r1, op1), Mov(rd, r2)) =>
-                // Remove redundance Mov Instructions
-                peepMov(r1, op1, rd, r2, remainingTail, optimised)
-                case (Mov(r1, op1), Cmp(rd, op2)) =>
-                // Check for redundant compare branches
-                peepBranch(r1, op1, rd, op2, remainingTail, optimised)
-                case (Ldr(r1, op1), Ldr(r2, op2)) =>
-                // Potential strong operation
-                peepStrong(r1, op1, r2, op2, remainingTail, optimised)
-                case (Str(r1, op1), Ldr(r2, op2)) =>
-                // Potential useless Load
-                peepStrLdr(r1, op1, r2, op2, remainingTail, optimised, Not_Byte)
-                case (StrB(r1, op1), LdrSB(r2, op2)) =>
-                // Potential useless Load
-                peepStrLdr(r1, op1, r2, op2, remainingTail, optimised, Is_Byte)
-                case _ =>
-                continueOptimise(cur, remainingHead, remainingTail, optimised)
-            }
-        }
-    }
+  /* Overloaded optimise function that is recursively called to store
+    optimised instructions in OPTIMISED */
+  def optimise(
+      cur: Instr,
+      instructions: mutable.ListBuffer[Instr],
+      optimised: mutable.ListBuffer[Instr]
+  ): Unit = {
+      if (instructions.isEmpty) {
+          optimised += cur
+      } else {
+          val remainingHead = instructions.head
+          val remainingTail = instructions.tail
+          (cur, remainingHead) match {
+              case (Mov(r1, op1), Mov(rd, r2)) =>
+              // Remove redundance Mov Instructions
+              peepMov(r1, op1, rd, r2, remainingTail, optimised)
+              case (Mov(r1, op1), Cmp(rd, op2)) =>
+              // Check for redundant compare branches
+              peepBranch(r1, op1, rd, op2, remainingTail, optimised)
+              case (Ldr(r1, op1), Ldr(r2, op2)) =>
+              // Potential strong operation
+              peepStrong(r1, op1, r2, op2, remainingTail, optimised)
+              case (Str(r1, op1), Ldr(r2, op2)) =>
+              // Potential useless Load
+              peepStrLdr(r1, op1, r2, op2, remainingTail, optimised, Not_Byte)
+              case (StrB(r1, op1), LdrSB(r2, op2)) =>
+              // Potential useless Load
+              peepStrLdr(r1, op1, r2, op2, remainingTail, optimised, Is_Byte)
+              case _ =>
+              continueOptimise(cur, remainingHead, remainingTail, optimised)
+          }
+      }
+  }
 
-    /* Call optimise on all blocks necessary */
-    def optimiseBlocks(
-        blocks: List[(Label, BlockInstrs)]
-    ): List[(Label, BlockInstrs)] = {
-        val returnBlocks = mutable.ListBuffer.empty[(Label, BlockInstrs)]
-        for (b <- blocks) {
-            val (Label(name), _) = b
-            // Ignore blocks that are not required
-            if (!ignoreBlocks.contains(Label(name))) {
-                name.take(2) match {
-                // Not necassary to optimise predefined blocks
-                    case "p_" => returnBlocks += b
-                    case _    => returnBlocks += optimiseBlock(b)
-                }
-            }
-        }
-        returnBlocks.toList
-    }
+  /* Call optimise on all blocks necessary */
+  def optimiseBlocks(
+      blocks: List[(Label, BlockInstrs)]
+  ): List[(Label, BlockInstrs)] = {
+      val returnBlocks = mutable.ListBuffer.empty[(Label, BlockInstrs)]
+      for (b <- blocks) {
+          val (Label(name), _) = b
+          // Ignore blocks that are not required
+          if (!ignoreBlocks.contains(Label(name))) {
+              name.take(2) match {
+              // Not necassary to optimise predefined blocks
+                  case "p_" => returnBlocks += b
+                  case _    => returnBlocks += optimiseBlock(b)
+              }
+          }
+      }
+      returnBlocks.toList
+  }
 
-    /* Removes redundant branches */
-    def peepBranch(
-        r1: Register,
-        op1: Operand,
-        rd: Register,
-        op2: Operand,
-        remainingTail: mutable.ListBuffer[Instr],
-        optimised: mutable.ListBuffer[Instr]
-    ): Unit = {
-        if (r1 == rd) {
-            (op1, op2) match {
-                // Comparison is true
-                case (Imm_Int(FALSE_INT), Imm_Int(FALSE_INT)) |
-                    (Imm_Int(TRUE_INT), Imm_Int(TRUE_INT)) =>
-                    remainingTail.head match {
-                    case BranchCond(EQ, label) =>
-                        // Branch to necessary label
-                        optimised += Branch(label)
-                    case _ =>
-                        continueOptimise(
-                        Mov(r1, op1),
-                        Cmp(rd, op2),
-                        remainingTail,
-                        optimised
-                        )
-                    }
-                // Comparison is false
-                case (Imm_Int(FALSE_INT), Imm_Int(TRUE_INT)) |
-                    (Imm_Int(TRUE_INT), Imm_Int(FALSE_INT)) =>
-                remainingTail.head match {
-                    case BranchCond(EQ, label) =>
-                    // LABEL will never be required
-                    ignoreBlocks += label
-                    // Optimise rest of the instructions
-                    optimise(remainingTail.tail, optimised)
-                    case _ =>
-                    continueOptimise(
-                        Mov(r1, op1),
-                        Cmp(rd, op2),
-                        remainingTail,
-                        optimised
-                    )
-                }
-                case _ =>
-                continueOptimise(Mov(r1, op1), Cmp(rd, op2), remainingTail, optimised)
-            }
-        } else {
-            continueOptimise(Mov(r1, op1), Cmp(rd, op2), remainingTail, optimised)
-        }
-    }
+  /* Removes redundant branches */
+  def peepBranch(
+      r1: Register,
+      op1: Operand,
+      rd: Register,
+      op2: Operand,
+      remainingTail: mutable.ListBuffer[Instr],
+      optimised: mutable.ListBuffer[Instr]
+  ): Unit = {
+      if (r1 == rd) {
+          (op1, op2) match {
+              // Comparison is true
+              case (Imm_Int(FALSE_INT), Imm_Int(FALSE_INT)) |
+                  (Imm_Int(TRUE_INT), Imm_Int(TRUE_INT)) =>
+                  remainingTail.head match {
+                  case BranchCond(EQ, label) =>
+                      // Branch to necessary label
+                      optimised += Branch(label)
+                  case _ =>
+                      continueOptimise(
+                      Mov(r1, op1),
+                      Cmp(rd, op2),
+                      remainingTail,
+                      optimised
+                      )
+                  }
+              // Comparison is false
+              case (Imm_Int(FALSE_INT), Imm_Int(TRUE_INT)) |
+                  (Imm_Int(TRUE_INT), Imm_Int(FALSE_INT)) =>
+              remainingTail.head match {
+                  case BranchCond(EQ, label) =>
+                  // LABEL will never be required
+                  ignoreBlocks += label
+                  // Optimise rest of the instructions
+                  optimise(remainingTail.tail, optimised)
+                  case _ =>
+                  continueOptimise(
+                      Mov(r1, op1),
+                      Cmp(rd, op2),
+                      remainingTail,
+                      optimised
+                  )
+              }
+              case _ =>
+              continueOptimise(Mov(r1, op1), Cmp(rd, op2), remainingTail, optimised)
+          }
+      } else {
+          continueOptimise(Mov(r1, op1), Cmp(rd, op2), remainingTail, optimised)
+      }
+  }
 
-    /* Remove redundant Mov(r1,op1), Mov(rd, r1) -> Mov(rd, op1) */
-    def peepMov(
-        r1: Register,
-        op1: Operand,
-        rd: Register,
-        r2: Operand,
-        instructions: mutable.ListBuffer[Instr],
-        optimised: mutable.ListBuffer[Instr]
-    ): Unit = {
-        if (r1 == r2) {
-        // Check destReg != op1
-            if (rd != op1) {
-                optimise(Mov(rd, op1), instructions, optimised)
-            } else {
-                // If rd == op1, no need to add intructions
-                optimise(instructions.head, instructions.tail, optimised)
-            }
-        } else {
-        continueOptimise(Mov(r1, op1), Mov(rd, r2), instructions, optimised)
-        }
-    }
+  /* Remove redundant Mov(r1,op1), Mov(rd, r1) -> Mov(rd, op1) */
+  def peepMov(
+      r1: Register,
+      op1: Operand,
+      rd: Register,
+      r2: Operand,
+      instructions: mutable.ListBuffer[Instr],
+      optimised: mutable.ListBuffer[Instr]
+  ): Unit = {
+      if (r1 == r2) {
+      // Check destReg != op1
+          if (rd != op1) {
+              optimise(Mov(rd, op1), instructions, optimised)
+          } else {
+              // If rd == op1, no need to add intructions
+              optimise(instructions.head, instructions.tail, optimised)
+          }
+      } else {
+      continueOptimise(Mov(r1, op1), Mov(rd, r2), instructions, optimised)
+      }
+  }
 
-    /* Removes redundant Load instructions */
-    def peepStrLdr(
-        r1: Register,
-        op1: Address,
-        r2: Register,
-        op2: Operand,
-        instructions: mutable.ListBuffer[Instr],
-        optimised: mutable.ListBuffer[Instr],
-        isByte: Boolean
-    ): Unit = {
-        if (r1 == r2 && op1 == op2) {
-            // Skip over the Load intruction
-            optimise(Str(isByte, r1, op1), instructions, optimised)
-        } else {
-            continueOptimise(
-            Str(isByte, r1, op1),
-            Ldr(isByte, r2, op2),
-            instructions,
-            optimised
-            )
-        }
-    }
+  /* Removes redundant Load instructions */
+  def peepStrLdr(
+      r1: Register,
+      op1: Address,
+      r2: Register,
+      op2: Operand,
+      instructions: mutable.ListBuffer[Instr],
+      optimised: mutable.ListBuffer[Instr],
+      isByte: Boolean
+  ): Unit = {
+      if (r1 == r2 && op1 == op2) {
+          // Skip over the Load intruction
+          optimise(Str(isByte, r1, op1), instructions, optimised)
+      } else {
+          continueOptimise(
+          Str(isByte, r1, op1),
+          Ldr(isByte, r2, op2),
+          instructions,
+          optimised
+          )
+      }
+  }
 
-    val LOG_ERROR = -1
-    private val Shift_Factor = 2.0
-    private val Drop_Branch_Insts = 3
+  val LOG_ERROR = -1
+  private val Shift_Factor = 2.0
+  private val Drop_Branch_Insts = 3
 
-    /* Get log2 of i, returns LOG_ERROR if I == 0 or value is not an Integer */
-    def getShiftAmount(i: Int): Int = {
-        if (i <= 0) {
-            LOG_ERROR
-        } else {
-            val dVal = log(i) / log(Shift_Factor)
-            if (dVal == floor(dVal)) {
-                dVal.toInt
-            } else {
-                LOG_ERROR
-            }
-        }
-    } 
+  /* Get log2 of i, returns LOG_ERROR if I == 0 or value is not an Integer */
+  def getShiftAmount(i: Int): Int = {
+      if (i <= 0) {
+          LOG_ERROR
+      } else {
+          val dVal = log(i) / log(Shift_Factor)
+          if (dVal == floor(dVal)) {
+              dVal.toInt
+          } else {
+              LOG_ERROR
+          }
+      }
+  } 
 
     /* Reduces strength of a division if possible */
   def divisionReduc(
